04439ca3cef37930d5dfa6fbce89ebc7c8cbf79b
riccardofano <rcr.fano@gmail.com>
2020-08-06 20:13:38 +0200

    add layout sizing

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,22 @@
 import { useUser } from "../utils/db/useUser";
 import { UserContext } from "../utils/types";
+import styled from "styled-components";
 
 import Navbar from "./NavBar";
+
+const StyledLayout = styled.main`
+  margin: 0 auto;
+  padding: 0 35px;
+
+  @media (min-width: 800px) {
+    max-width: 768px;
+    padding: 0;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1160px;
+    padding: 0;
+  }
+`;
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -12,9 +27,11 @@ export default function Layout({ children, url }: LayoutProps) {
   const { user, logout } = useUser();
 
   return (
-    <UserContext.Provider value={{ user, logout }}>
-      <Navbar url={url} />
-      {children}
-    </UserContext.Provider>
+    <StyledLayout>
+      <UserContext.Provider value={{ user, logout }}>
+        <Navbar url={url} />
+        {children}
+      </UserContext.Provider>
+    </StyledLayout>
   );
 }
