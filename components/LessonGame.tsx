fd840051153b52fe7b0feadb572a90dcdcfdde3a
riccardofano <rcr.fano@gmail.com>
2020-08-09 17:08:06 +0200

    add start typing button

--- components/LessonGame.tsx
+++ components/LessonGame.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState, MouseEvent } from "react";
 import { useImmerReducer } from "use-immer";
+import styled, { keyframes } from "styled-components";
 
 import { StateContext, IState, ICharacter, LessonInfo } from "../utils/types";
 import { keyHandler } from "../utils/keyHandler";
@@ -12,6 +13,39 @@ import Result from "./Result";
 import Chart from "./Chart";
 import ProgressBar from "./ProgressBar";
 import { Button } from "./Buttons";
+
+const ResetButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem;
+`;
+
+const appearAnimation = keyframes`
+  0% {
+    opacity: 0;
+    transform: translate(-50%, -50px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translate(-50%, 0);
+  }
+`;
+
+const StartNotice = styled.span`
+  position: fixed;
+  top: 7rem;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 16px;
+  text-align: center;
+  color: #fff;
+  background: linear-gradient(90deg, #4ba8ec 0%, #0f57c2 100%);
+  padding: 0.5rem 2rem;
+  border-radius: 20px;
+
+  animation: ${appearAnimation} 500ms ease-in-out;
+`;
 
 interface LessonGameProps {
   info: LessonInfo;
@@ -59,21 +93,29 @@ export default function LessonGame({ info, initialState }: LessonGameProps) {
         {state.text.length === state.currentPosition ? (
           <>
             <Result {...info} dispatch={dispatch} />
+            <ResetButtonContainer>
+              <Button onClick={handleReset}>Reset</Button>
+            </ResetButtonContainer>
             <Chart lessonId={info.id} />
           </>
         ) : (
-          <div style={{ width: "max-content", margin: "0 auto" }}>
-            <ProgressBar
-              length={(state.currentPosition / state.text.length) * 100}
-            />
-            <LessonText
-              lines={lines}
-              currentLine={state.currentLine}
-              lineOffset={2}
-            />
-          </div>
+          <>
+            {!state.started && <StartNotice>Start typing</StartNotice>}
+            <div style={{ width: "max-content", margin: "9rem auto 0" }}>
+              <ProgressBar
+                length={(state.currentPosition / state.text.length) * 100}
+              />
+              <LessonText
+                lines={lines}
+                currentLine={state.currentLine}
+                lineOffset={2}
+              />
+              <ResetButtonContainer>
+                <Button onClick={handleReset}>Reset</Button>
+              </ResetButtonContainer>
+            </div>
+          </>
         )}
-        <Button onClick={handleReset}>Reset</Button>
       </StateContext.Provider>
     </div>
   );
