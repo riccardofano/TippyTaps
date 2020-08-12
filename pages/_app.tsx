ea6f3d6d6f093bcce4441ab02405a74eb8a382ae
riccardofano <rcr.fano@gmail.com>
2020-08-12 14:40:29 +0200

    add useDarkMode hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,22 @@
 import { AppProps } from "next/app";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 
 import { GlobalStyles } from "../styles/global";
 import { light, dark } from "../styles/themes";
+import { useDarkMode } from "../utils/useDarkMode";
 
 export const ThemeToggle = createContext(
-  {} as { toggle: () => void; theme: "light" | "dark" }
+  {} as { toggle: () => void; theme: string }
 );
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
-  const toggleTheme = (): void => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  };
+  const { theme, toggle } = useDarkMode();
 
   return (
     <ThemeProvider theme={theme === "light" ? light : dark}>
       <GlobalStyles />
-      <ThemeToggle.Provider value={{ toggle: toggleTheme, theme }}>
+      <ThemeToggle.Provider value={{ toggle, theme }}>
         <Component {...pageProps} />
       </ThemeToggle.Provider>
     </ThemeProvider>
