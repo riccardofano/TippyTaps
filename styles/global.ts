e9f4919134747f18e7512c3db2e6d080c19dbf7c
riccardofano <rcr.fano@gmail.com>
2020-08-24 09:20:31 +0200

    add local fonts

--- styles/global.ts
+++ styles/global.ts
@@ -4,6 +4,29 @@ import { normalize } from "styled-normalize";
 export const GlobalStyles = createGlobalStyle`
   ${normalize}
 
+  @font-face {
+    font-family: "Rubik";
+    font-weight: 400;
+    src: url("fonts/Rubik-Regular.ttf");
+  }
+
+  @font-face {
+    font-family: "Rubik";
+    font-weight: 700;
+    src: url("fonts/Rubik-Bold.ttf");
+  }
+
+  @font-face {
+    font-family: "Rubik";
+    font-weight: 300;
+    src: url("fonts/Rubik-Light.ttf");
+  }
+
+  @font-face {
+    font-family: "Roboto Mono";
+    src: url("fonts/RobotoMono-Regular.ttf");
+  }
+
   body {
     background: ${({ theme }) => theme.colors.background};
     color: ${({ theme }) => theme.colors.text.main};
